1b467edcc49e05abfc07a57a409996b0c1fc15a8
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-07-21 17:31:23 -0300

    feat: some buttons styles

--- src/pages/FavoriteRecipes/index.js
+++ src/pages/FavoriteRecipes/index.js
@@ -26,27 +26,29 @@ export default function FavoriteRecipes() {
   return (
     <div>
       <Header />
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-        onClick={ () => setFilter('') }
-      >
-        All
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-food-btn"
-        onClick={ () => setFilter('comida') }
-      >
-        Food
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-        onClick={ () => setFilter('bebida') }
-      >
-        Drinks
-      </button>
+      <div className="navigation">
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilter('') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilter('comida') }
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilter('bebida') }
+        >
+          Drinks
+        </button>
+      </div>
       { favoriteRecipes && filterFavoriteRecipesType(filter, favoriteRecipes)
         .map((recipe, index) => (
           <FavoriteRecipeCard
